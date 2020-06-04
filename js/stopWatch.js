4359364bbad8605538fa192c9de9d36a61eb3cf3
Sunker <lhhoang98197@gmail.com>
2020-06-04 23:30:17 +0700

    complete stopwatch, but not css

--- js/stopWatch.js
+++ js/stopWatch.js
@@ -4,36 +4,63 @@ const second = document.querySelector('#second');
 const milisecond = document.querySelector('#milisecond');
 const btnStart = document.querySelector('#start-btn');
 const btnReset = document.querySelector('#reset-btn');
+const logTime = document.querySelector('.log-time');
 
 let currentTime = 0;
 let stop = false;
 
 let timer = setInterval(function() {
-    currentTime += 10;
-    let appendMilisecond = parseInt((currentTime % 1000) / 10);
-    let appendSecond = Math.floor((currentTime / 1000) % 60);
-    let appendMinute = Math.floor((currentTime / (1000 * 60)) % 60);
-    let appendHours = Math.floor((currentTime / (1000 * 60 * 60)) % 24);
+    if(stop) {
+        currentTime += 10;
+        let appendMilisecond = parseInt((currentTime % 1000) / 10);
+        let appendSecond = Math.floor((currentTime / 1000) % 60);
+        let appendMinute = Math.floor((currentTime / (1000 * 60)) % 60);
+        let appendHours = Math.floor((currentTime / (1000 * 60 * 60)) % 24);
 
-    appendMilisecond = appendMilisecond < 10 ? '0' + appendMilisecond : appendMilisecond;
-    milisecond.innerHTML = appendMilisecond;
+        appendMilisecond = appendMilisecond < 10 ? '0' + appendMilisecond : appendMilisecond;
+        milisecond.innerHTML = appendMilisecond;
 
-    appendSecond = appendSecond < 10 ? '0' + appendSecond : appendSecond;
-    second.innerHTML = appendSecond;
+        appendSecond = appendSecond < 10 ? '0' + appendSecond : appendSecond;
+        second.innerHTML = appendSecond;
 
-    appendMinute = appendMinute < 10 ? '0' + appendMinute : appendMinute;
-    minute.innerHTML = appendMinute;
+        appendMinute = appendMinute < 10 ? '0' + appendMinute : appendMinute;
+        minute.innerHTML = appendMinute;
 
-    appendHours = appendHours < 10 ? '0' + appendHours : appendHours;
-    hours.innerHTML = appendHours;
+        appendHours = appendHours < 10 ? '0' + appendHours : appendHours;
+        hours.innerHTML = appendHours;
+    }
 }, 10);
 
 btnStart.addEventListener('click', function(e) {
     e.preventDefault();
-    stop = false;
-    btnStart.value = stop;
+    if(stop) {
+        stop = false;
+        btnStart.value = "start";
+        console.log(stop);
+        logStopTime();
+    } else {
+        stop = true;
+        btnStart.value = "stop";
+        console.log(stop);
+    }
 });
 
 
+let resetTime = function() {
+    hours.innerHTML = '00';
+    minute.innerHTML = '00';
+    second.innerHTML = '00';
+    milisecond.innerHTML = '00';
+}
 
+btnReset.addEventListener('click', resetTime);
 
+let logStopTime = function() {
+    console.log(hours.textContent + ':' + minute.textContent + ':' + second.textContent + ':' + milisecond.textContent);
+    let contentLogTime = document.createElement('p');
+    contentLogTime.innerHTML = hours.textContent + ':' + minute.textContent + ':' + second.textContent + ':' + milisecond.textContent;
+    logTime.appendChild(contentLogTime);
+    
+}
+
+resetTime();
